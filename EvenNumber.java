cd5d8175cf59d755def1f7d59db8962a63fa7e67
Swati Sharma <swati.sharma.7819@gmail.com>
2024-08-25 13:55:21 +0530

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int min, max;
+        System.out.println("This application finds all the even number between the range of [min, max] !");
+        System.out.print("Enter the minimum point: ");
+        min = sc.nextInt();
+        System.out.print("Enter the maximum point: ");
+        max = sc.nextInt();
+
+        for (int i = min; i <= max; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+
+        }
+        sc.close();
+
+    }
+
+}
+
