63d9e221380b668101a33108f3b27e649d9b85c6
Swati Sharma <swati.sharma.7819@gmail.com>
2024-09-19 11:27:46 +0530

    Add files via upload

---
+++ ASCIIValue.java
@@ -0,0 +1,9 @@
+public class ASCIIValue {
+    public static void main(String[] args) {
+        char ch1 = 'a', ch2 = 'R' ;
+        System.out.println((int)ch1);
+        System.out.println((int)ch2);
+
+    }
+    
+}
