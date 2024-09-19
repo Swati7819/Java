99166bf6a8b1b8f441e3c1cb701661104583f67e
Swati Sharma <swati.sharma.7819@gmail.com>
2024-09-19 11:08:20 +0530

    Add files via upload

---
+++ PatternProblem3.java
@@ -0,0 +1,24 @@
+public class PatternProblem3 {
+
+    public static void main(String[] args) {
+        System.out.println("Pyramid Star ");
+
+        for(int i =1 ;i<=5;i++)
+        {
+            for(int j=1; j<=(5-i); j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int k=1; k<=i; k++)
+            {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+
+        }
+    
+    }
+    
+}
